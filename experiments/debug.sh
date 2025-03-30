e40a5e0b789f09238b117a17a54d6b450d38a956
William-Paul Lacroix <wlacroix@submit.coli.uni-saarland.de>
2025-03-30 15:51:31 +0200

    infer working

--- experiments/debug.sh
+++ experiments/debug.sh
@@ -36,9 +36,13 @@ echo "Begin Training"
 llamafactory-cli train experiments/debug.yaml \
 > experiments/logs/debug_train.log  2>&1
 
+echo "Begin Merge"
+llamafactory-cli export experiments/debug_merge.yaml \
+> experiments/logs/debug_merge.log  2>&1
+
 echo "Begin Inference"
-python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/debug" --save_path "/scratch/wlacroix/.cache/llama_factory/debug" --template llama3 --dataset debug \
-> experiments_sarubi/logs/debug_infer.log  2>&1
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/wlacroix/.cache/llama_factory/debug" --save_path "/scratch/wlacroix/.cache/llama_factory/debug" --template llama3 --dataset debug \
+> experiments/logs/debug_infer.log  2>&1
 
 #or if you encounter error:
 #FORCE_TORCHRUN=1 PTA/experiments_sarubi/llama3_lora_sft.yaml \
