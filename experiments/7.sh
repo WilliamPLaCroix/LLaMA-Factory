92603b97da19f72a4f2122f7a2914b52e7defac6
William-Paul Lacroix <wlacroix@submit.coli.uni-saarland.de>
2025-04-11 15:58:49 +0200

    fix infer ppl, add fkgl / loss / ppl to compute_metrics

--- experiments/7.sh
+++ experiments/7.sh
@@ -20,13 +20,13 @@ which python
 # Main Experiment Script
 echo "Starting Main Experiment Workflow!"
 
-echo "Begin Training"
-llamafactory-cli train experiments/7.yaml \
-> experiments/logs/7_train.log  2>&1
+#echo "Begin Training"
+#llamafactory-cli train experiments/7.yaml \
+#> experiments/logs/7_train.log  2>&1
 
-echo "Begin Merge"
-llamafactory-cli export experiments/7_merge.yaml \
-> experiments/logs/7_merge.log  2>&1
+#echo "Begin Merge"
+#llamafactory-cli export experiments/7_merge.yaml \
+#> experiments/logs/7_merge.log  2>&1
 
 echo "Begin Inference"
 python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/7_adapter" --save_path "/scratch/wlacroix/.cache/llama_factory/7" --template llama3 --dataset wikilarge_grade_7_test --temperature 0 --grade 7 \
