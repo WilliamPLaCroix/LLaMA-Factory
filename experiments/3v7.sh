73b5740d9b6e3a7443ae469ce6dcc12eaf630885
WilliamPLaCroix <williamplacroix@gmail.com>
2025-04-14 21:02:34 +0200

    add 3v7 and 11v7 subs

--- experiments/3v7.sh
+++ experiments/3v7.sh
@@ -22,18 +22,22 @@ echo "Starting Main Experiment Workflow!"
 
 # echo "Begin Training"
 # llamafactory-cli train experiments/3.yaml \
-# > experiments/logs/3_train.log  2>&1
+# > experiments/logs/3_train.log 2>&1
 
 # echo "Begin Merge"
 # llamafactory-cli export experiments/3_merge.yaml \
-# > experiments/logs/3_merge.log  2>&1
+# > experiments/logs/3_merge.log 2>&1
 
 echo "Begin Inference"
-python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/3_adapter" --save_path "/scratch/wlacroix/.cache/llama_factory/3" --template llama3 --dataset wikilarge_grade_7_test --temperature 0 --grade 7 \
-> experiments/logs/3_infer.log  2>&1
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/3_adapter/checkpoint-6040" --save_path "/scratch/wlacroix/.cache/llama_factory/3" --template llama3 --dataset wikilarge_grade_3_test --temperature 0 --grade 3 \
+> experiments/logs/3v7_infer.log 2>&1
+
+# echo "Begin Inference"
+# python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --save_path "/scratch/wlacroix/.cache/llama_factory/debug" --template llama3 --dataset debug --temperature 0 \
+# > experiments/logs/debug_7_infer.log 2>&1
 
 #or if you encounter error:
 #FORCE_TORCHRUN=1 PTA/experiments_sarubi/llama3_lora_sft.yaml \
-#> PTA/experiments_sarubi/logs_lora_sft  2>&1
+#> PTA/experiments_sarubi/logs_lora_sft 2>&1
 
 echo "Main Experiment Workflow Completed!"
