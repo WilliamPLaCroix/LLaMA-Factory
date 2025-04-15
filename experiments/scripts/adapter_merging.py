a24ad3ebe48f3afd1b42f886732aed159d366cef
WilliamPLaCroix <williamplacroix@gmail.com>
2025-04-15 23:24:41 +0200

    remove density for linear merge

--- experiments/scripts/adapter_merging.py
+++ experiments/scripts/adapter_merging.py
@@ -36,7 +36,7 @@ def main(model="/scratch/common_models/Llama-3.2-3B-Instruct",
         _ = model.load_adapter(adapter_path, adapter_name=grade)
         print("Loading adapter from:", adapter_path)
     merged_adapter_name = "_merge_".join(grades)
-    model.add_weighted_adapter(adapters, weights, merge_method=merge_method, density=density, adapter_name=merged_adapter_name)
+    model.add_weighted_adapter(adapters, weights, merge_method=merge_method, adapter_name=merged_adapter_name)#, density=density)
     
     # clean up unused adapters
     for grade in grades:
@@ -48,8 +48,8 @@ def main(model="/scratch/common_models/Llama-3.2-3B-Instruct",
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Merge models and adapters")
-    parser.add_argument("--adapters", nargs="+", help="List of adapter paths", default=None)
-    parser.add_argument("--grades", nargs="+", help="List of adapter grades. len must match --adapters", default=None)
+    parser.add_argument("--adapters", nargs="+", help="List of adapter paths", default=None, required=True)
+    parser.add_argument("--grades", nargs="+", help="List of adapter grades. len must match --adapters", default=None, required=True)
     parser.add_argument("--model", type=str, help="Path to the model", default=None)
     # add adapter weights as list
     parser.add_argument("--weights", nargs="+", help="List of adapter weights", default=None)
