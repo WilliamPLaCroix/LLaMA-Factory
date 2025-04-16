e80fc8501e9f761188abca55bf1da21aba452e3a
William-Paul Lacroix <wlacroix@submit.coli.uni-saarland.de>
2025-04-16 14:57:38 +0200

    backup recent changes for dare_ties merge+infer

--- experiments/3_merge_11.sh
+++ experiments/3_merge_11.sh
@@ -24,15 +24,14 @@ echo "Starting Main Experiment Workflow!"
 #llamafactory-cli train experiments/3_11.yaml \
 #> experiments/logs/3_11_train.log 2>&1
 
-#echo "Begin Merge"
+echo "Begin Merge"
 #llamafactory-cli export experiments/3_11_merge.yaml \
-#python3 experiments/scripts/adapter_merging.py --adapters='["/scratch/wlacroix/.cache/llama_factory/3_adapter", "/scratch/wlacroix/.cache/llama_factory/11_adapter"]' --grades='[3, 11]' --weights='[1, 1]' \
-#> experiments/logs/3_merge_11_merge.log 2>&1
-
+python3 experiments/scripts/adapter_merging.py --adapters='["/scratch/wlacroix/.cache/llama_factory/3_adapter", "/scratch/wlacroix/.cache/llama_factory/11_adapter"]' --grades='[3, 11]' --weights='[1, 1]' --merge_method dare_ties --density 0.5 \
+> experiments/logs/3_merge_11_merge.log 2>&1
 
 echo "Begin Inference"
 python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/3_merge_11_adapter" --save_path "/scratch/wlacroix/.cache/llama_factory/3_merge_11_adapter" --template llama3 --dataset wikilarge_grade_7_test --temperature 0 --grade 7 \
-> experiments/logs/3_merge_11_infer.log 2>&1
+> experiments/logs/3_dareties_11_infer.log 2>&1
 
 #or if you encounter error:
 #FORCE_TORCHRUN=1 PTA/experiments_sarubi/llama3_lora_sft.yaml \
