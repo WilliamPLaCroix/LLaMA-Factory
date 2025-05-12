308d39a56a19d0a5c85fe8df396b3da90014a5a5
WilliamPLaCroix <williamplacroix@gmail.com>
2025-05-12 15:33:55 +0200

    move scripts to new folder

---
+++ old_experiments/3.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source /nethome/wlacroix/LLaMA-Factory/experiments/scripts/rename_gpus.sh
+source /nethome/wlacroix/miniconda3/etc/profile.d/conda.sh
+echo "Current conda environment: $CONDA_DEFAULT_ENV"
+conda activate /nethome/wlacroix/miniconda3/envs/llama_factory_v2
+echo "Activated conda environment: $CONDA_DEFAULT_ENV"
+cd /nethome/wlacroix/LLaMA-Factory
+
+# Debugging: Check CUDA details
+echo "=== CUDA Debugging Information ==="
+nvcc --version
+nvidia-smi
+echo "CUDA_HOME: $CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+echo "==================================="
+echo "HOSTNAME: $HOSTNAME"
+which python
+
+# Main Experiment Script
+echo "Starting Main Experiment Workflow!"
+
+echo "Begin Training"
+llamafactory-cli train experiments/3.yaml \
+> experiments/logs/3_train.log 2>&1
+
+echo "Begin Merge"
+llamafactory-cli export experiments/3_merge.yaml \
+> experiments/logs/3_merge.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/3_adapter" --save_path "/scratch/wlacroix/.cache/llama_factory/3" --template llama3 --dataset wikilarge_grade_3_test --temperature 0 --grade 3 \
+> experiments/logs/3_infer.log 2>&1
+
+#or if you encounter error:
+#FORCE_TORCHRUN=1 PTA/experiments_sarubi/llama3_lora_sft.yaml \
+#> PTA/experiments_sarubi/logs_lora_sft 2>&1
+
+echo "Main Experiment Workflow Completed!"
