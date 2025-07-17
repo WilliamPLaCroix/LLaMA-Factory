b0f9d1250b1e3be9bbe6584ea484b4fb8a604b74
William-Paul Lacroix <wlacroix@submit.coli.uni-saarland.de>
2025-07-17 20:54:09 +0200

    prep 2_through_12_dareties.sh for inference

--- experiments/2_through_12_dareties.sh
+++ experiments/2_through_12_dareties.sh
@@ -20,18 +20,53 @@ which python
 # Main Experiment Script
 echo "Starting Main Experiment Workflow!"
 
-#echo "Begin Training"
-#llamafactory-cli train experiments/3_11.yaml \
-#> experiments/logs/3_11_train.log 2>&1
-
 echo "Begin Merge"
-#llamafactory-cli export experiments/3_11_merge.yaml \
-#python3 experiments/scripts/adapter_merging.py --adapters='["/scratch/wlacroix/.cache/llama_factory/2_adapter", "/scratch/wlacroix/.cache/llama_factory/3_adapter", "/scratch/wlacroix/.cache/llama_factory/4_adapter", "/scratch/wlacroix/.cache/llama_factory/5_adapter", "/scratch/wlacroix/.cache/llama_factory/6_adapter", "/scratch/wlacroix/.cache/llama_factory/7_adapter", "/scratch/wlacroix/.cache/llama_factory/8_adapter", "/scratch/wlacroix/.cache/llama_factory/9_adapter", "/scratch/wlacroix/.cache/llama_factory/10_adapter", "/scratch/wlacroix/.cache/llama_factory/11_adapter", "/scratch/wlacroix/.cache/llama_factory/12_adapter"]' --grades='[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]' --weights='[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]' --merge_method dare_ties --density 0.5 \
-#> experiments/logs/2_through_12_dareties_merge.log 2>&1
+python3 experiments/scripts/adapter_merging.py --adapters='["/scratch/wlacroix/.cache/llama_factory/2_adapter", "/scratch/wlacroix/.cache/llama_factory/3_adapter", "/scratch/wlacroix/.cache/llama_factory/4_adapter", "/scratch/wlacroix/.cache/llama_factory/5_adapter", "/scratch/wlacroix/.cache/llama_factory/6_adapter", "/scratch/wlacroix/.cache/llama_factory/7_adapter", "/scratch/wlacroix/.cache/llama_factory/8_adapter", "/scratch/wlacroix/.cache/llama_factory/9_adapter", "/scratch/wlacroix/.cache/llama_factory/10_adapter", "/scratch/wlacroix/.cache/llama_factory/11_adapter", "/scratch/wlacroix/.cache/llama_factory/12_adapter"]' --grades='[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]' --weights='[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]' --merge_method dare_ties --density 0.5 \
+> experiments/logs/2_through_12_dareties_merge.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_2_test --temperature 0 --grade 2 \
+> experiments/logs/2_through_12_dareties_infer_2.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_3_test --temperature 0 --grade 3 \
+> experiments/logs/2_through_12_dareties_infer_3.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_4_test --temperature 0 --grade 4 \
+> experiments/logs/2_through_12_dareties_infer_4.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_5_test --temperature 0 --grade 5 \
+> experiments/logs/2_through_12_dareties_infer_5.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_6_test --temperature 0 --grade 6 \
+> experiments/logs/2_through_12_dareties_infer_6.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_7_test --temperature 0 --grade 7 \
+> experiments/logs/2_through_12_dareties_infer_7.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_8_test --temperature 0 --grade 8 \
+> experiments/logs/2_through_12_dareties_infer_8.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_9_test --temperature 0 --grade 9 \
+> experiments/logs/2_through_12_dareties_infer_9.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_10_test --temperature 0 --grade 10 \
+> experiments/logs/2_through_12_dareties_infer_10.log 2>&1
+
+echo "Begin Inference"
+python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_11_test --temperature 0 --grade 11 \
+> experiments/logs/2_through_12_dareties_infer_11.log 2>&1
 
 echo "Begin Inference"
 python3 scripts/vllm_infer_metrics.py --model_name_or_path "/scratch/common_models/Llama-3.2-3B-Instruct" --adapter_name_or_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --save_path "/scratch/wlacroix/.cache/llama_factory/2_through_12_dareties" --template llama3 --dataset wikilarge_grade_12_test --temperature 0 --grade 12 \
-> experiments/logs/2_through_12_dareties_infer.log 2>&1
+> experiments/logs/2_through_12_dareties_infer_12.log 2>&1
 
 
 echo "Main Experiment Workflow Completed!"
