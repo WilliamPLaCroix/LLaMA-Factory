91528509e57b88ebab0a8fd9f35a160f53ceb0c2
WilliamPLaCroix <williamplacroix@gmail.com>
2025-11-16 21:23:08 +0100

    iter 94..95

--- experiments/baseline.sh
+++ experiments/baseline.sh
@@ -59,7 +59,7 @@ nvidia-smi || true; nvcc --version || true
 set -euo pipefail
 
 # for loop to iterate through evals by ITERATION
-for ITERATION_NUM in {92..93}; do
+for ITERATION_NUM in {94..95}; do
     ITERATION="-${ITERATION_NUM}"
     echo "Starting experiment for iteration: ${ITERATION_NUM}"
     RUN_KEY="${MODEL_VARIATION}-${BASE_GROUP}-v1${ITERATION}"
@@ -92,29 +92,29 @@ for ITERATION_NUM in {92..93}; do
 
     # --------------- manual eval ---------------
     echo "[train] will now run llamafactory-cli train ${CFG}"
-    echo "starting manual eval"
-    export WANDB_JOB_TYPE="eval"
-    llamafactory-cli train \
-    --model_name_or_path /scratch/common_models/Llama-3.2-3B-Instruct-greedy \
-    --adapter_name_or_path "${OUT_ADAPTER}" \
-    --trust_remote_code True \
-    --template llama3 \
-    --do_train False \
-    --do_eval True \
-    --finetuning_type lora \
-    --eval_dataset cleaned_baseline_validation \
-    --output_dir "${LOG_DIR}" \
-    --overwrite_output_dir True \
-    --cutoff_len 1024 \
-    --seed 42 \
-    --per_device_eval_batch_size 32 \
-    --bf16 True \
-    --predict_with_generate False \
-    --do_sample False \
-    --report_to wandb \
-    --run_name "${WANDB_NAME}" \
-    > "${LOG_DIR}/cleaned_baseline_validation${ITERATION}_eval.log" 2>&1
-    echo "[eval] completed eval for iteration ${ITERATION} into run ${WANDB_RUN_ID}"
+    # echo "starting manual eval"
+    # export WANDB_JOB_TYPE="eval"
+    # llamafactory-cli train \
+    # --model_name_or_path /scratch/common_models/Llama-3.2-3B-Instruct-greedy \
+    # --adapter_name_or_path "${OUT_ADAPTER}" \
+    # --trust_remote_code True \
+    # --template llama3 \
+    # --do_train False \
+    # --do_eval True \
+    # --finetuning_type lora \
+    # --eval_dataset cleaned_baseline_validation \
+    # --output_dir "${LOG_DIR}" \
+    # --overwrite_output_dir True \
+    # --cutoff_len 1024 \
+    # --seed 42 \
+    # --per_device_eval_batch_size 32 \
+    # --bf16 True \
+    # --predict_with_generate False \
+    # --do_sample False \
+    # --report_to wandb \
+    # --run_name "${WANDB_NAME}" \
+    # > "${LOG_DIR}/cleaned_baseline_validation${ITERATION}_eval.log" 2>&1
+    # echo "[eval] completed eval for iteration ${ITERATION} into run ${WANDB_RUN_ID}"
 
     # --------------- INFER (same run; tag infer dataset + grade) ---------------
     echo "starting vllm eval"
